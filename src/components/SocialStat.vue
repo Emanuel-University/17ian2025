cd21c0d536fa5f34949eaf6d9f5b355d6debaf21
Radu Crisan <radu.crisan@ekkode.com>
2025-01-17 15:38:50 +0200

    add routes

--- src/components/SocialStat.vue
+++ src/components/SocialStat.vue
@@ -3,6 +3,7 @@ import TwitterLogo from "../assets/twitter.webp"
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const props = defineProps(["info"])
+const lsSocial = JSON.parse(localStorage.getItem("socialInfo"));
 
 let logo = undefined;
 import { onBeforeMount } from "vue"
@@ -16,7 +17,7 @@ onBeforeMount(() => {
 
 <template>
     <div v-if="props.info" class="w-full flex flex-col bg-orange-200 p-2 rounded-md drop-shadow-md">
-        <div class="flex"> <img :src="logo" alt="Twitter" class="h-8"> {{ props.info.title }} </div>
+        <div class="flex"> <img :src="logo" alt="Twitter" class="h-8"> {{ props.info.title }} <span v-if="lsSocial" class="bg-blue-200"> {{ lsSocial.title }}</span></div>
         <div> {{ props.info.price }} </div>
         <div> <i class="bi bi-arrow-bar-up"></i> {{ props.info.sensText }}</div>
         <div> {{ props.info.trialsText }} </div>
---
+++ src/pages/SocialStatPage.vue
@@ -0,0 +1,46 @@
+<script setup>
+import SocialStat from '../components/SocialStat.vue';
+
+localStorage.setItem("socialInfo", JSON.stringify({ title: 'Twitter' }));
+
+const twitterJSON = {
+    logo: "twitter",
+    title: "Twitter",
+    price: "$500 000",
+    sensIcon: "up",
+    sensText: "+40% more impressions",
+    trialsText: "5% new trials"
+}
+
+const facbookJSON = {
+    logo: "facebok",
+    title: "Facebok",
+    price: "$500 000",
+    sensIcon: "up",
+    sensText: "+40% more impressions",
+    trialsText: "5% new trials"
+}
+
+const tiktokJSON = {
+    logo: "twitter",
+    title: "Facebok",
+    price: "$500 000",
+    sensIcon: "up",
+    sensText: "+40% more impressions",
+    trialsText: "5% new trials"
+}
+
+</script>
+
+<template>
+    <div class="container mx-auto">
+        <div class="gap-6 grid grid-cols-3">
+            <SocialStat :info="twitterJSON" />
+            <SocialStat :info="facbookJSON" />
+            <SocialStat :info="tiktokJSON" />
+            <SocialStat :info="twitterJSON" />
+            <SocialStat :info="twitterJSON" />
+            <SocialStat :info="twitterJSON" />
+        </div>
+    </div>
+</template>
